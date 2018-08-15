b9202794e35bf8c7bc2dce331a79a3ae93a07cbe
top <kritkrai.rak@bru.ac.th>
2018-08-16 04:06:20 +0700

    Initial commit

---
+++ src/main/java/com/thanachart/demo/BankThanachartApplication.java
@@ -0,0 +1,12 @@
+package com.thanachart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankThanachartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankThanachartApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thanachart/demo/BankThanachartApplicationTests.java
@@ -0,0 +1,21 @@
+package com.thanachart.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BankThanachartApplicationTests.class)
+@WebAppConfiguration
+public class BankThanachartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
